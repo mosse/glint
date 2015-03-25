48c4ee3621f9201bf5d2cd8c5367e6944132be67
Chris Rinaldi <cgrinaldi@gmail.com>
2015-03-24 21:24:23 -0700

    (feat) Sends username with idea upvote

--- client/app/votes/votes.js
+++ client/app/votes/votes.js
@@ -3,13 +3,14 @@
 
 angular.module('glint.votes', [])
 
-.controller('VotesCtrl', function(Votes){
+.controller('VotesCtrl', function($window, Votes){
   var self = this;
 
   // Display the user's upvotes and pass them along to the db.
   self.upvote = function(idea, gCount){
     var ideaRef = idea;
-    idea = {idea: idea, gCount: gCount};
+    var username = JSON.parse($window.localStorage.getItem('com.glinted')).data.username;
+    idea = {idea: idea, gCount: gCount, username: username};
     idea = JSON.stringify(idea);
     Votes.upvote(idea)
       .then(function (response){
