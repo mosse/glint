ade4dcee64784cabbcfc5ad6efd5c71722fbd8af
Chris Rinaldi <cgrinaldi@gmail.com>
2015-03-25 11:37:18 -0700

    (feat) Adds skeleton for future work on boards router

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -6,9 +6,9 @@
 // Allows for parsing of POST request body.
 var bodyParser  = require('body-parser');
 // Error logging and handling helper functions.
-var helpers = require('./helpers.js'); 
+var helpers = require('./helpers.js');
 // Logs requests sent from the client.
-var morgan = require('morgan'); 
+var morgan = require('morgan');
 
 module.exports = function (app, express) {
 
@@ -17,6 +17,7 @@ module.exports = function (app, express) {
   var voteRouter = express.Router();
   var commentRouter = express.Router();
   var usersRouter = express.Router();
+  var boardsRouter = express.Router();
 
   // Associate the Express server app with the different modules that it should use.
   app.use(bodyParser.urlencoded({extended: true}));
@@ -27,18 +28,21 @@ module.exports = function (app, express) {
   app.use(helpers.handleErrors);
 
   // Use the idea router for all idea requests.
-  app.use('/api/ideas', ideaRouter); 
+  app.use('/api/ideas', ideaRouter);
   // Use vote router for requests related to upvoting or downvoting.
   app.use('/api/vote', voteRouter);
   // Use comment router for requests related to adding and getting comments.
   app.use('/api/comments', commentRouter);
   // Use users router for requests related to adding and getting user data.
   app.use('/api/users', usersRouter);
+  // Use boards router for requests related to viewing a private board.
+  app.use('/api/boards', boardsRouter);
 
   // Inject our Express routers into their respective route files.
   require('../ideas/ideaRoutes.js')(ideaRouter);
   require('../votes/voteRoutes.js')(voteRouter);
   require('../comments/commentRoutes.js')(commentRouter);
   require('../users/usersRoutes.js')(usersRouter);
+  require('../boards/boardsRouter.js')(boardsRouter);
 
 };
