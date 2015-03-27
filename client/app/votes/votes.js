986bdef5a72a622eed62d8c58bce06ef29cf697c
garrettjoecox <garrettjcox@gmail.com>
2015-03-27 10:55:01 -0700

    (feat) Adds error messages for G input

--- client/app/votes/votes.js
+++ client/app/votes/votes.js
@@ -6,17 +6,19 @@ angular.module('glint.votes', [])
 .controller('VotesCtrl', function($window, Votes, Auth, $location){
   var self = this;
   self.gCount = 1;
+  self.message = "How many G's?";
+  self.hasError = false;
 
   // Display the user's upvotes and pass them along to the db.
   self.upvote = function(idea){
+    if (!Auth.user) { $location.path('/login'); return; }
     var gCount = +self.gCount;
-    if (!Auth.user){
-      $location.path('/login');
-      return;
-    }
-    if (Auth.user.wallet-gCount < 0 || Auth.user.username === idea.created_by){
-      return;
-    }
+    if (isNaN(gCount)) { self.hasError = true; self.message = "That's not a number!"; return; }
+    if (gCount < 1) { self.hasError = true; self.message = "It needs to be more than 1..."; return; }
+    if (Auth.user.wallet-gCount < 0) { self.hasError = true; self.message = "You don't have enough G's!"; return; }
+    if (Auth.user.username === idea.created_by) { self.hasError = true; self.message = "You can't give yourself G's!"; return; }
+    self.hasError = false;
+    self.message = "How many G's?";
     var ideaRef = idea;
     idea = {idea: idea, gCount: gCount, username: Auth.user.username};
     idea = JSON.stringify(idea);
